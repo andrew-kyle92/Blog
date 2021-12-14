a921fd3be0419a44cfbef0d76cf3da67018d22ed
Andrew Kyle <andrew.kyle92@yahoo.com>
2021-12-14 15:35:11 +0000

    Changed some things

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,24 @@
+function closeMessage(){
+    var message = document.querySelector(".email-success");
+    message.remove()
+}
 
-    function closeMessage(){
-        var message = document.querySelector(".email-success");
-        message.remove()
-    }
+function image(){
+    // Get the modal
+    var modal = document.getElementById("img-popup");
 
-    function image(){
-        // Get the modal
-        var modal = document.getElementById("img-popup");
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("profileImg");
+    var modalImg = document.getElementById("img01");
 
-        // Get the image and insert it inside the modal - use its "alt" text as a caption
-        var img = document.getElementById("profileImg");
-        var modalImg = document.getElementById("img01");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
 
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
+function SpanClick() {
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close-btn")[0];
 
-    function SpanClick() {
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close-btn")[0];
-
-        // When the user clicks on <span> (x), close the modal
-        modal.style.display = "none";
-    }
+    // When the user clicks on <span> (x), close the modal
+    modal.style.display = "none";
+}
